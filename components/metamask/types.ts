f2d256f113a894268cab8d9ed13c82e896adb96e
SamuraiT <t.yasukawa01@gmail.com>
2022-03-25 01:16:46 +0900

    feat: Introduce web3-reack and implemented Connect, add network and add henkaku token

---
+++ components/metamask/types.ts
@@ -0,0 +1,23 @@
+interface Network {
+  chainId: string;
+  rpcUrls: string[];
+  chainName: string;
+  nativeCurrency: {
+    name: string;
+    symbol: string;
+    decimals: number
+  },
+  blockExplorerUrls: string[]
+}
+
+interface TokenInfo {
+  type: string;
+  options: {
+    address: string;
+    symbol: string;
+    decimals: number;
+    image: string;
+  }
+}
+
+export { Network, TokenInfo }
